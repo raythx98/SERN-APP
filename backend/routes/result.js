0717013be6bcb74dbd61ad01e6a4b8c4dafd0fed
raythx98 <65889977+raythx98@users.noreply.github.com>
2022-07-14 14:39:49 +0800

    getHistory API

--- backend/routes/result.js
+++ backend/routes/result.js
@@ -1,19 +1,72 @@
 const router = require("express").Router();
+const { query } = require("express");
 var db = require("./database");
 
 router.get("/getHistory", (req, res) => {
-//   db.query(
-//     `SELECT *`,
-//     [name, insert_id, group, tpoint, tgoal, apoint, rdate],
-//     (err, result) => {
-//       if (err) {
-//         console.log(err);
-//       } else {
-//         console.log(result);
-//         res.send(result);
-//       }
-//     }
-//   );
+  db.query(
+    "SELECT result_id FROM results WHERE is_visible = true ORDER BY result_id DESC",
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).json("Database error...");
+      } else {
+        console.log(result);
+        let query_string = "";
+        let query_size= 0;
+
+        const result_ids = result.map((row) => {
+          query_size++;
+          query_string += `
+          (select tname, gnum, tpoint, tgoal, apoint, rdate 
+            from teamresult 
+            where result_id = ${row.result_id} 
+            and gnum = (select (select min(gnum) from teamresult where result_id=${row.result_id}))
+            order by  tpoint DESC, tgoal DESC, apoint DESC, rdate ASC
+            limit 4 )
+            UNION
+            (select tname, gnum, tpoint, tgoal, apoint, rdate 
+            from teamresult 
+            where result_id = ${row.result_id} 
+            and gnum = (select (select max(gnum) from teamresult where result_id=${row.result_id}))
+            order by tpoint DESC, tgoal DESC, apoint DESC, rdate ASC 
+            limit 4 );
+          `;
+        });
+        console.log(result_ids);
+
+        if (!query_size) {
+          return res.send([]);
+        }
+
+        db.query(query_string,
+          (err, result) => {
+            if (err) {
+              console.log(err);
+              return res
+                .status(500)
+                .json("Database error after insertion, please refresh...");
+            } else {
+              console.log(result);
+              result.forEach((element, index) => {
+                element.forEach((innerElement, innerIndex) => {
+                 const date_obj = new Date(element.rdate)
+                 new_date = date_obj.getDate().toString().padStart(2, '0');
+                 new_month = (date_obj.getMonth()+1).toString().padStart(2, '0');
+                 element.rdate = new_date + '/' + new_month;
+                });
+              });
+              console.log(result);
+              if (query_size === 1) {
+                res.send([result]);
+              } else {
+                res.send(result);
+              }
+            }
+          }
+        );
+      }
+    }
+  );
 });
 
 router.delete("/clearHistory", (req, res) => {
